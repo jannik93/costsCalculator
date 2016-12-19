33bd75ae0a70e33110d6a9dc0e712bc91cfda607
Jannik B <Jannik B>
2016-12-19 15:06:02 +0100

    added

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    <title>Start</title>
+    
+    <link href="theme/bootstrap-3.3.7/css/bootstrap.css" rel="stylesheet"/>
+    <link href="theme/css/default.css" rel="stylesheet" /> 
+
+    <script href="theme/bootstrap-3.3.7/js/bootstrap.js" />
+
+    </head>
+
+</html>
+
+<?php
+
+
+
+?>
