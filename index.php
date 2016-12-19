1b5ee61aa9b0d31800e44323920116ea2aa821c2
Jannik B <Jannik B>
2016-12-19 15:50:38 +0100

    built base

--- index.php
+++ index.php
@@ -8,11 +8,22 @@
     <script href="theme/bootstrap-3.3.7/js/bootstrap.js" />
 
     </head>
+    <body>
+     <ul class="nav nav-tabs">
+  <li class="active"><a data-toggle="tab" href="#home">Home</a></li>
+  <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+</ul>
 
+<div class="tab-content">
+  <div id="home" class="tab-pane fade in active">
+    <h3>HOME</h3>
+    <p>Some content.</p>
+  </div>
+  <div id="menu1" class="tab-pane fade">
+    <h3>Menu 1</h3>
+    <p>Some content in menu 1.</p>
+  </div>
+</div> 
+    </bod>
 </html>
 
-<?php
-
-
-
-?>
