185dde8bf2770b70af0e436f9e990c82b16a918c
Jannik <jannik93.b@gmail.com>
2017-01-10 12:20:14 +0100

    change month language

--- summary.php
+++ summary.php
@@ -49,18 +49,18 @@ echo "</table>";
 function get_month_name($month)
 {
     $months = array(
-        1   =>  'January',
-        2   =>  'February',
-        3   =>  'March',
+        1   =>  'Januar',
+        2   =>  'Februar',
+        3   =>  'März',
         4   =>  'April',
-        5   =>  'May',
-        6   =>  'June',
-        7   =>  'July',
+        5   =>  'Mai',
+        6   =>  'Juni',
+        7   =>  'Juli',
         8   =>  'August',
         9   =>  'September',
-        10  =>  'October',
+        10  =>  'Oktober',
         11  =>  'November',
-        12  =>  'December'
+        12  =>  'Dezember'
     );
 
     return $months[$month];
