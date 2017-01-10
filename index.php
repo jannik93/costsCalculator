b42bb97b782460c2538203c03d86475893ee107f
Jannik <jannik93.b@gmail.com>
2017-01-10 22:21:22 +0100

    Update index.php Added scale

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <head>
 
         <meta charset="utf-8"/>
+         
+        <meta name="viewport"      content="width=device-width, initial-scale=1.0">
 
         <script src="theme/bootstrap-3.3.7/js/bootstrap.js"></script>
         <script src="theme/bootstrap-3.3.7/js/npm.js"></script>
